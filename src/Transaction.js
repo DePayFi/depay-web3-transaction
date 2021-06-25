3daa347c8b5a772a3ef7d75f1adf98682c6404c1
Sebastian Pape <s@pape.io>
2021-06-25 13:05:47 +0200

    fix lint

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,49 +1,52 @@
 import submitEthereum from './ethereum/submit'
 
 class Transaction {
-
   constructor({ blockchain, address, api, method, params, sent, confirmed, safe }) {
-    this.blockchain = blockchain;
-    this.address = address;
-    this.api = api;
-    this.method = method;
-    this.params = params;
-    this.sent = sent;
-    this.confirmed = confirmed;
-    this.safe = safe;
-    this._confirmed = false;
-    this._safe = false;
+    this.blockchain = blockchain
+    this.address = address
+    this.api = api
+    this.method = method
+    this.params = params
+    this.sent = sent
+    this.confirmed = confirmed
+    this.safe = safe
+    this._confirmed = false
+    this._safe = false
   }
 
   confirmation() {
-    if(this._confirmed) { return Promise.resolve(this) }
+    if (this._confirmed) {
+      return Promise.resolve(this)
+    }
     return new Promise((resolve, reject) => {
-      let originalConfirmed = this.confirmed;
-      this.confirmed = ()=>{
-        if(originalConfirmed) originalConfirmed(this)
+      let originalConfirmed = this.confirmed
+      this.confirmed = () => {
+        if (originalConfirmed) originalConfirmed(this)
         resolve(this)
       }
     })
   }
 
   safeConfirmation() {
-    if(this._safe) { return Promise.resolve(this) }
+    if (this._safe) {
+      return Promise.resolve(this)
+    }
     return new Promise((resolve, reject) => {
-      let originalSafe = this.safe;
-      this.safe = ()=>{
-        if(originalSafe) originalSafe(this)
+      let originalSafe = this.safe
+      this.safe = () => {
+        if (originalSafe) originalSafe(this)
         resolve(this)
       }
-    }) 
+    })
   }
 
   submit() {
-    switch(this.blockchain) {
+    switch (this.blockchain) {
       case 'ethereum':
-        return submitEthereum(this);
-      break;
+        return submitEthereum(this)
+        break
       default:
-        throw('BlockchainTransaction: Unknown blockchain');
+        throw 'BlockchainTransaction: Unknown blockchain'
     }
   }
 }
