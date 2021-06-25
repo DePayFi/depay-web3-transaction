3386d11ba8931f4fe1e4d428863362bf6d6284c2
Sebastian Pape <s@pape.io>
2021-06-25 13:09:33 +0200

    0.9.0

--- dist/es/index.js
+++ dist/es/index.js
@@ -8,33 +8,35 @@ function submitEthereum (transaction) {
     let fragment = contract.interface.fragments.find((fragment) => {
       return fragment.name == transaction.method
     });
-    
+
     let args = fragment.inputs.map((input) => {
       return transaction.params[input.name]
     });
 
-    contract.connect(signer)[transaction.method](...args).then((sentTransaction)=>{
-      if(sentTransaction) {
-        transaction.id = sentTransaction.hash;
-        if(transaction.sent) transaction.sent(transaction);
-        sentTransaction.wait(1).then(()=>{
-          transaction._confirmed = true;
-          if(transaction.confirmed) transaction.confirmed(transaction);
-        });
-        sentTransaction.wait(12).then(()=>{
-          transaction._safe = true;
-          if(transaction.safe) transaction.safe(transaction);
-        });
-        resolve(transaction);
-      } else {
-        reject('BlockchainTransaction: No transaction has been submitted!');
-      }
-    });
+    contract
+      .connect(signer)
+      [transaction.method](...args)
+      .then((sentTransaction) => {
+        if (sentTransaction) {
+          transaction.id = sentTransaction.hash;
+          if (transaction.sent) transaction.sent(transaction);
+          sentTransaction.wait(1).then(() => {
+            transaction._confirmed = true;
+            if (transaction.confirmed) transaction.confirmed(transaction);
+          });
+          sentTransaction.wait(12).then(() => {
+            transaction._safe = true;
+            if (transaction.safe) transaction.safe(transaction);
+          });
+          resolve(transaction);
+        } else {
+          reject('BlockchainTransaction: No transaction has been submitted!');
+        }
+      });
   })
 }
 
 class Transaction {
-
   constructor({ blockchain, address, api, method, params, sent, confirmed, safe }) {
     this.blockchain = blockchain;
     this.address = address;
@@ -49,33 +51,37 @@ class Transaction {
   }
 
   confirmation() {
-    if(this._confirmed) { return Promise.resolve(this) }
+    if (this._confirmed) {
+      return Promise.resolve(this)
+    }
     return new Promise((resolve, reject) => {
       let originalConfirmed = this.confirmed;
-      this.confirmed = ()=>{
-        if(originalConfirmed) originalConfirmed(this);
+      this.confirmed = () => {
+        if (originalConfirmed) originalConfirmed(this);
         resolve(this);
       };
     })
   }
 
   safeConfirmation() {
-    if(this._safe) { return Promise.resolve(this) }
+    if (this._safe) {
+      return Promise.resolve(this)
+    }
     return new Promise((resolve, reject) => {
       let originalSafe = this.safe;
-      this.safe = ()=>{
-        if(originalSafe) originalSafe(this);
+      this.safe = () => {
+        if (originalSafe) originalSafe(this);
         resolve(this);
       };
-    }) 
+    })
   }
 
   submit() {
-    switch(this.blockchain) {
+    switch (this.blockchain) {
       case 'ethereum':
-        return submitEthereum(this);
+        return submitEthereum(this)
       default:
-        throw('BlockchainTransaction: Unknown blockchain');
+        throw 'BlockchainTransaction: Unknown blockchain'
     }
   }
 }
